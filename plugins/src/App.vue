524c79d2437bc74a3ac034129e1caa4bbbc2f802
Sushchenok Mykhailo <78119979+QuemS@users.noreply.github.com>
2023-02-26 11:36:36 +0200

    add new method sceleton

--- plugins/src/App.vue
+++ plugins/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
+    <div class="title"><h1>Suspense method</h1></div>
     <card-user-sceleton />
+    <div class="title"><h1>Stable method</h1></div>
+    <div>
+      <profile-card-skeleton v-if="loader" />
+      <card-user :get-loader="getLoader" v-show="!loader"></card-user>
+    </div>
     <v-loader />
   </div>
 </template>
@@ -8,13 +14,34 @@
 <script>
 import cardUserSceleton from './components/cardUserSceleton.vue'
 import VLoader from './components/VLoader.vue'
+import cardUser from './components/newSceleton/cardUser.vue'
+import ProfileCardSkeleton from './components/ProfileCardSkeleton.vue'
 
 export default {
+  data() {
+    return {
+      iten: '',
+      loader: true
+    }
+  },
+  methods: {
+    getLoader(value) {
+      console.log(value)
+      this.loader = value
+    }
+  },
+
   components: {
     cardUserSceleton,
-    VLoader
+    VLoader,
+    cardUser,
+    ProfileCardSkeleton
   }
 }
 </script>
 
-<style></style>
+<style>
+.title {
+  text-align: center;
+}
+</style>
