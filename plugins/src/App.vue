408e30c6a1868bf80d55f3e958e7b2ba0cdec500
Sushchenok Mykhailo <78119979+QuemS@users.noreply.github.com>
2023-02-26 11:41:46 +0200

    fix eroor

--- plugins/src/App.vue
+++ plugins/src/App.vue
@@ -7,7 +7,8 @@
       <profile-card-skeleton v-if="loader" />
       <card-user :get-loader="getLoader" v-show="!loader"></card-user>
     </div>
-    <v-loader />
+    <div class="title"><h1>Loaded style</h1></div>
+    <v-loader class="content_center" />
   </div>
 </template>
 
@@ -43,5 +44,9 @@ export default {
 <style>
 .title {
   text-align: center;
+}
+.content_center {
+  display: flex;
+  justify-content: center;
 }
 </style>
